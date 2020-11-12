53791e6e6ccf5dbcf2ab4269a1a2b0b3e3e7495e
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-11-12 15:10:32 +0000

    feito trabalho realizado

--- resources/views/jogadores/show.blade.php
+++ resources/views/jogadores/show.blade.php
@@ -9,7 +9,7 @@ Jogador
 <ul>
 ID Jogador: {{$jogador->id_jogador}}<br>
 Nome: {{$jogador->nome}}<br>
-Nacionalidade: {{$jogador->nacionalidade}}<br>
+Nacionalidade: {{$jogador->nacionalidade}} <i class="{{strtolower($jogador->nacionalidade)}} flag"></i><br>
 
 @if(isset ($jogador->equipas->designacao))
         Equipa: {{$jogador->equipas->designacao}}<br>
